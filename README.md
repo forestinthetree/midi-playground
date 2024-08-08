a0d417a9392a57e47ed483ea5fb7cdba192954d5
Tak Tran <contact@tutaktran.com>
2024-08-08 21:53:01 +0100

    Initial setup of astro files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Grid EN16 Playground
+
+## Development
+
+1. `npm install`
+1. `npm run dev`
---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import mkcert from 'vite-plugin-mkcert';
+
+import react from "@astrojs/react";
+
+const { PUBLIC_SITE_URL } = process.env;
+
+// https://astro.build/config
+export default defineConfig({
+  site: PUBLIC_SITE_URL,
+  vite: {
+    plugins: [mkcert()],
+    server: {
+      https: true
+    },
+  },
+  integrations: [react()]
+});
